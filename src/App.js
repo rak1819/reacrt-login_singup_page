6a2ae8c4f3d75aa1ddfea1728656802bffedacf2
rakeshjeethuri@gmail.com <rakeshjeethuri@gmail.com>
2025-02-08 14:05:13 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Loginsignup from './componenets/assests/login-signup/Loginsignup';
+
+
+function App() {
+  return (
+    <div>
+      <Loginsignup/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componenets/assests/login-signup/Loginsignup.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import './loginsignup.css'
+import person from './person.png'
+import email from './emailnew.png'
+import password from './password.png'
+export default function Loginsignup() {
+    const[action,setaction] = useState("Login");
+  return (
+    <div className='Container'>
+        <div className="header">
+            <div className="text">{action}</div>
+            <div className="underline"> </div>
+        </div>
+        <div className="inputs">
+            {action==="Login"?<div className></div>:<div className="input">
+                <img className='im' src={person} alt="name" />
+                <input type="text" placeholder="Name"name="" id="" />
+            </div>}
+
+            <div className="input">
+                <img className='im' src={email} alt="" />
+                <input type="email" placeholder="Email id"name="" id="" />
+            </div>
+            <div className="input">
+                <img className='im' src={password} alt="" />
+                <input type="password" placeholder='password' name="" id="" />
+            </div>
+        </div>
+        <div className="forget-password">Lost Password ?<span>Click Here</span></div>
+        <div className="submit_container">
+            <div className={action==="Login"?"submit grey":"submit"} onClick={()=>{setaction("Signup")}}>signup</div>
+            <div className={action==="Signup"?"submit grey":"submit"} onClick ={()=>{setaction("Login")}}>Login</div>
+        </div>
+    </div>
+  )
+}
